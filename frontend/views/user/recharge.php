21ebff7cecd0031fe8c5203e1a8293cd02daa400
Liang Jian <15934854815@163.com>
2018-01-29 15:11:30 +0800

    pay

--- frontend/views/user/recharge.php
+++ frontend/views/user/recharge.php
@@ -196,6 +196,7 @@
         <div class="moneyhead">充值方式</div>
     </div>
     <div class="payType">
+		<!--
         <div class="boxflex1 paystyle checkImg2" style="border-top:0;"  data-type="1">
             <img src="/images/icon-chat.png" style="width: 20px;">
             <span>微信扫码</span>
@@ -207,6 +208,7 @@
             <span>支付宝扫码</span>
             <img src="/images/notseleted.png" alt="" style="float:right;" class="check-payone checkPay" >
         </div>
+		-->
        <div class="boxflex1 paystyle checkImg1 qqsaoma"  data-type="3">
             <img src="/images/qqpay.png" style="width: 20px;">
             <span>QQ扫码</span>
